0adfbcce7b52bf0776109d39f6b9d74ce749da6e
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-25 15:09:17 -0400

    authmodal stub

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,8 @@
+import { useDisclosure } from "@chakra-ui/react";
+import React from "react";
+
+const AuthModal: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return <></>;
+};
+export default AuthModal;
