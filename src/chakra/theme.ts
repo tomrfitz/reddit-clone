7d922eda0fbbee4734657d533a8ffc75bc7c0c21
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-25 14:31:25 -0400

    adding globals.css brought back navbar

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,7 +1,4 @@
 import { extendTheme } from "@chakra-ui/react";
-import "fontsource/open-sans/300.css";
-import "fontsource/open-sans/400.css";
-import "fontsource/open-sans/700.css";
 
 export const theme = extendTheme({
   colors: {
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,22 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 const Navbar: React.FC = () => {
   console.log("Navbar");
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex></Flex>
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="redditFace" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          alt="redditText"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+      {/* <Directory />
+          <SearchInput />
+          <RightContext/> */}
     </Flex>
   );
 };
