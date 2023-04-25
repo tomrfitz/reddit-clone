884b973575e490e63030c121af9fbbd58dce58a4
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-25 16:34:16 -0400

    log in -> login

--- src/components/Modal/Auth/Signup.tsx
+++ src/components/Modal/Auth/Signup.tsx
@@ -3,22 +3,19 @@ import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 
-type LoginProps = {};
-
-const Login: React.FC<LoginProps> = () => {
-  const [loginForm, setLoginForm] = useState({
+const Signup: React.FC = () => {
+  const [signupForm, setSignupForm] = useState({
     email: "",
     password: "",
   });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setSignupForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   // firebase logic
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(loginForm);
   };
 
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -65,11 +62,29 @@ const Login: React.FC<LoginProps> = () => {
         }}
         bg="gray.50"
       />
+      <Input
+        required
+        name="confirm password"
+        placeholder="confirm password"
+        type="password"
+        mb={2}
+        onChange={onChange}
+        fontSize={"10pt"}
+        _placeholder={{ color: "gray.500" }}
+        _hover={{ border: "1px solid", borderColor: "blue.500", bg: "white" }}
+        _focus={{
+          outline: "none",
+          border: "1px solid",
+          borderColor: "blue.500",
+          bg: "white",
+        }}
+        bg="gray.50"
+      />
       <Button width="100%" height="36px" type="submit" mb={2} mt={2}>
-        Login
+        Sign Up
       </Button>
       <Flex fontSize="9pt" justifyContent="center">
-        <Text mr={1}>New here?</Text>
+        <Text mr={1}>Already have an account?</Text>
         <Text
           color="blue.500"
           cursor="pointer"
@@ -77,14 +92,14 @@ const Login: React.FC<LoginProps> = () => {
           onClick={() =>
             setAuthModalState((prev) => ({
               ...prev,
-              view: "signup",
+              view: "login",
             }))
           }
         >
-          SIGN UP
+          LOG IN
         </Text>
       </Flex>
     </form>
   );
 };
-export default Login;
+export default Signup;
