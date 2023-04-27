e9bd3608f63c6abf8d1c4b20fa17a00ded4cabeb
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-27 15:13:57 -0400

    start UserMenu

---
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -0,0 +1,65 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import React from "react";
+import { CgProfile } from "react-icons/cg";
+import { FaRedditSquare } from "react-icons/fa";
+import { VscAccount } from "react-icons/vsc";
+
+type UserMenuProps = {
+  user?: User | null;
+};
+
+const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  return (
+    <Menu>
+      <MenuButton
+        cursor={"pointer"}
+        padding="0px 6px"
+        borderRadius={4}
+        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+      >
+        {user ? (
+          <Flex align="center">
+            <Flex align="center">
+              <>
+                <Icon
+                  fontSize={24}
+                  mr={1}
+                  color="gray.300"
+                  as={FaRedditSquare}
+                />
+              </>
+              <ChevronDownIcon />
+            </Flex>
+          </Flex>
+        ) : (
+          <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
+        )}
+      </MenuButton>
+      <MenuList>
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{
+            bg: "blue.500",
+            color: "white",
+          }}
+        >
+          <Flex align="center">
+            <Icon as={CgProfile} fontSize={20} mr={2} />
+            Profile
+          </Flex>
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+export default UserMenu;
