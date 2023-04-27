2e2945aae2a0c65654c88f15e41433c8c7a3e127
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-27 14:39:10 -0400

    remove logout button, replace with Icons component

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,9 +1,9 @@
-import { auth } from "@/src/firebase/clientApp";
-import { Button, Flex } from "@chakra-ui/react";
-import { User, signOut } from "firebase/auth";
+import { Flex } from "@chakra-ui/react";
+import { User } from "firebase/auth";
 import React from "react";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
 
 type RightContentProps = {
   user?: User | null;
@@ -14,17 +14,7 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
     <>
       <AuthModal />
       <Flex justify="center" align="center">
-        {user ? (
-          <Button
-            onClick={() => {
-              signOut(auth);
-            }}
-          >
-            Logout
-          </Button>
-        ) : (
-          <AuthButtons />
-        )}
+        {user ? <Icons /> : <AuthButtons />}
       </Flex>
     </>
   );
