fcb679a8183052458c53931e6f722402ed80fb70
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-27 14:25:06 -0400

    signout button to rightContent

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,18 +1,30 @@
-import { Flex } from "@chakra-ui/react";
+import { auth } from "@/src/firebase/clientApp";
+import { Button, Flex } from "@chakra-ui/react";
+import { signOut } from "firebase/auth";
 import React from "react";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
 
 type RightContentProps = {
-  //user:
+  user?: any;
 };
 
-const RightContent: React.FC<RightContentProps> = () => {
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
       <AuthModal />
       <Flex justify="center" align="center">
-        <AuthButtons />
+        {user ? (
+          <Button
+            onClick={() => {
+              signOut(auth);
+            }}
+          >
+            Logout
+          </Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
