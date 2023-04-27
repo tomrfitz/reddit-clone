92d39a073e9a9bad42c6388154e4dd79b1873c75
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-27 14:34:59 -0400

    import firebase User type

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,12 +1,12 @@
 import { auth } from "@/src/firebase/clientApp";
 import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { User, signOut } from "firebase/auth";
 import React from "react";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
 
 type RightContentProps = {
-  user?: any;
+  user?: User | null;
 };
 
 const RightContent: React.FC<RightContentProps> = ({ user }) => {
