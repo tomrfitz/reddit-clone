3445b8a376c9e35ff82fa9c59c0b47a1b18c2ce5
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-30 14:34:46 -0400

    loop to create tabs, empty event functions

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,8 +1,53 @@
-import React from "react";
+import { Flex, Icon } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { IoDocumentText, IoImageOutline } from "react-icons/io5";
+import TabItem from "./TabItem";
 
 type NewPostFormProps = {};
 
+const formTabs: tabItem[] = [
+  {
+    key: "post",
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    key: "images",
+    title: "Images & Video",
+    icon: IoImageOutline,
+  },
+];
+
+export type tabItem = {
+  key: string;
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
 const NewPostForm: React.FC<NewPostFormProps> = () => {
-  return <div>Have a good coding</div>;
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+
+  const handleCreatePost = async () => {};
+
+  const onSelectImage = () => {};
+
+  const onTextChange = () => {};
+
+  return (
+    <>
+      <Flex direction={"column"} borderRadius={4} mt={2} bg="white">
+        <Flex width="100%">
+          {formTabs.map((item) => (
+            <TabItem
+              item={item}
+              key={item.key}
+              selected={item.title === selectedTab}
+              setSelectedTab={setSelectedTab}
+            />
+          ))}
+        </Flex>
+      </Flex>
+    </>
+  );
 };
 export default NewPostForm;
