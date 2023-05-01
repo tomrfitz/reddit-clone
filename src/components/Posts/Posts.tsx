bdf0978463ad96cb3d9c00d6b8decff20267806d
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-30 22:39:32 -0400

    Posts logic

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,66 @@
+import { Community } from "@/src/atoms/communitiesAtom";
+import { Post } from "@/src/atoms/postsAtom";
+import { auth, firestore } from "@/src/firebase/clientApp";
+import usePosts from "@/src/hooks/usePosts";
+import { Stack } from "@chakra-ui/react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import PostItem from "./PostItem";
+
+type PostsProps = {
+  communityData: Community;
+};
+
+const Posts: React.FC<PostsProps> = ({ communityData }) => {
+  const [user] = useAuthState(auth);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onVote,
+    onSelectPost,
+    onDeletePost,
+  } = usePosts();
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState<string>("");
+
+  const getPosts = async () => {
+    try {
+      const postsQuery = query(
+        collection(firestore, "posts"),
+        where("communityId", "==", communityData.id),
+        orderBy("createdAt", "desc")
+      );
+      const postDocs = await getDocs(postsQuery);
+      const posts = postDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPostStateValue((prev) => ({ ...prev, posts: posts as Post[] }));
+    } catch (error: any) {
+      setError(error.message);
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <Stack>
+      {postStateValue.posts.map((post) => (
+        <PostItem
+          key={post.id}
+          post={post}
+          userIsCreator={user?.uid === post.creatorId}
+          userVoteValue={undefined}
+          onVote={onVote}
+          onSelectPost={onSelectPost}
+          onDeletePost={onDeletePost}
+        />
+      ))}
+    </Stack>
+  );
+};
+export default Posts;
