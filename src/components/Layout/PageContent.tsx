4c45fc9276a5f44f6e485e90b4f188ff3da911b0
Thomas FitzGerald <tomrfitz@gmail.com>
2023-05-01 16:16:29 -0400

    fix page spacing

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,34 +1,37 @@
-import { Flex } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
 
-type PageContentProps = {
-  children: ReactNode;
-};
+interface PageContentLayoutProps {
+  children: React.ReactNode;
+  maxWidth?: string;
+}
 
-const PageContent: React.FC<PageContentProps> = ({ children }) => {
+// Assumes array of two children are passed
+const PageContentLayout: React.FC<PageContentLayoutProps> = ({
+  children,
+  maxWidth,
+}) => {
   return (
-    <>
-      <Flex justify={"center"} p="16px 0px">
-        <Flex justify="center" width="95%" maxWidth={"860px"}>
-          {/* Lefthand side column */}
-          <Flex
-            direction="column"
-            width={{ base: "100%", md: "65" }}
-            mr={{ base: 0, md: 6 }}
-          >
-            {children && children[0 as keyof typeof children]}
-          </Flex>
-          {/* Righthand side column */}
-          <Flex
-            direction="column"
-            display={{ base: "none", md: "flex" }}
-            flexGrow={1}
-          >
-            {children && children[1 as keyof typeof children]}
-          </Flex>
+    <Flex justify="center" p="16px 0px">
+      <Flex width="95%" justify="center" maxWidth={maxWidth || "860px"}>
+        <Flex
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+        >
+          {children && children[0 as keyof typeof children]}
         </Flex>
+        {/* Right Content */}
+        <Box
+          display={{ base: "none", md: "flex" }}
+          flexDirection="column"
+          flexGrow={1}
+        >
+          {children && children[1 as keyof typeof children]}
+        </Box>
       </Flex>
-    </>
+    </Flex>
   );
 };
-export default PageContent;
+
+export default PageContentLayout;
