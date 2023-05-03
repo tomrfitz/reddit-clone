e1ec727ab63b3896fd16bf8e762034e03b0b2eef
Thomas FitzGerald <tomrfitz@gmail.com>
2023-05-02 22:46:16 -0400

    too many authmodals, disable in usePosts

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -24,11 +24,11 @@ const usePosts = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
   const router = useRouter();
 
-  useEffect(() => {
-    if (!user?.uid) {
-      setAuthModalState({ isOpen: true, view: "login" });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (!user?.uid) {
+  //     setAuthModalState({ isOpen: true, view: "login" });
+  //   }
+  // }, []);
 
   const onVote = async (
     event: React.MouseEvent<SVGElement, MouseEvent>,
